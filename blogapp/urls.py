09b07e92b076f8f9257e3ccf7e75b1f8d2702615
leesfood <notifications@leesfood.com>
2024-09-25 13:05:44 +0700

    update

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from blogapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', include('blogapp.urls')),  # Blog app URLs
-    path('', views.homepage, name='homepage'),  # Root URL points to homepage
+    path('blog/', include('blogapp.urls')),  # Includes the blogapp's URLs
 ]
