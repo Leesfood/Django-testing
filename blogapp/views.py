4dfe304f77be0286d146bb28cfc94a2d033d4a4f
leesfood <notifications@leesfood.com>
2024-09-25 14:15:06 +0700

    update

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render
 from .models import Post
-import pyrebase
-
-
 
 # Create your views here.
 
@@ -11,5 +8,6 @@ def index(request):
     posts = Post.objects.filter(trending=False)  # Fetch non-trending posts
 
     return render(request, 'blogapp/index.html', {'trending_posts': trending_posts, 'posts': posts})
+
 def readmore(request):
     return render(request, 'blogapp/readmore.html')
--- myblog/urls.py
+++ myblog/urls.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
+from blogapp import views  # Correct the import
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('readmore/', views.readmore, name='readmore'),
+]
+
+# Serve media files during development
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('blog/', include('blogapp.urls'))
-]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
